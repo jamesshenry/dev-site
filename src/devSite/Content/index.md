a09ab0000a9af33c8d516b6256a89a0728df0f45
jamesshenry <git@mail.jamesshenry.dev>
2026-03-30 13:51:35 +0100

    batman

---
+++ src/devSite/Content/index.md
@@ -0,0 +1,31 @@
+---
+title: "Content Site first post"
+description: "Description"
+date: 2025-03-31
+tags:
+
+- introduction
+- welcome
+isDraft: false
+---
+
+Welcome to my new content site! This is my first post using MyLittleContentEngine.
+
+## What is MyLittleContentEngine?
+
+MyLittleContentEngine is a static site generator built specifically for .NET Blazor applications. It allows you to:
+
+- Write content in Markdown
+- Use `dotnet watch` to develop your site
+- Generate static HTML for fast loading at deployment
+
+## Getting Started
+
+Creating content is as simple as writing Markdown files with YAML front matter. The engine handles the rest! Except the
+writing!
+
+```csharp
+var theAnswer = 30 + 25;
+```
+
+And that's it! You now have a basic content site up and running with MyLittleContentEngine.
---
+++ src/devSite/Program.cs
@@ -0,0 +1,59 @@
+using devSite.Components;
+using MyLittleContentEngine;
+using MyLittleContentEngine.Models;
+using MyLittleContentEngine.MonorailCss;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents();
+
+builder
+    .Services.AddContentEngineService(_ => new ContentEngineOptions
+    {
+        SiteTitle = "Test Site",
+        SiteDescription = "A test site using MyLittleContentEngine",
+        ContentRootPath = "Content",
+    })
+    .WithMarkdownContentService(_ => new MarkdownContentOptions<BlogFrontMatter>()
+    {
+        ContentPath = "Content",
+        BasePageUrl = string.Empty,
+    });
+
+builder.Services.AddMonorailCss();
+var app = builder.Build();
+
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>();
+app.UseMonorailCss();
+await app.RunOrBuildContent(args);
+
+public class BlogFrontMatter : IFrontMatter
+{
+    public string Title { get; init; } = "Empty title";
+    public string Description { get; init; } = string.Empty;
+    public string? Uid { get; init; } = null;
+
+    public DateTime Date { get; init; } = DateTime.Now;
+    public bool IsDraft { get; init; } = false;
+    public string[] Tags { get; init; } = [];
+    public string? RedirectUrl { get; init; }
+    public string? Section { get; init; }
+
+    public Metadata AsMetadata()
+    {
+        return new Metadata()
+        {
+            Title = Title,
+            Description = Description,
+            LastMod = Date,
+            RssItem = true,
+        };
+    }
+}
